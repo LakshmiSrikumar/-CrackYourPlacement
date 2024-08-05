647cde8865785b1bf72eb8b2464c37fe2870b057
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2024-08-05 21:51:53 +0530

    Create ProductArrayPuzzle.cpp

---
+++ ProductArrayPuzzle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+     long long n= nums.size();
+    vector<long long int>ans(n,1);
+     long long int left=1;
+     long long int right=1;
+     for (int i=0;i<n;i++){
+         ans[i]=left;
+         left*=nums[i];
+     }
+     for(int i=n-1;i>=0;i--){
+         ans[i]*=right;
+         right*=nums[i];
+     }
+      return ans;
+    }
+};
