6b984a85165f540a58e696bb3241df205596c99d
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2024-08-14 22:48:10 +0530

    Create Minimum Swaps to Sort.cpp

---
+++ Minimum Swaps to Sort.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums)
+	{
+	    vector<pair<int,int>>v;
+	    for(int i=0;i<nums.size();i++){
+	        v.push_back({nums[i],i});
+	    }
+	    sort(v.begin(),v.end());
+	    
+	    int swaps=0;
+	    
+	    for(int i=0;i<nums.size();i++){
+	        if(i!=(v[i].second)){
+	            swap(v[i],v[v[i].second]);
+	            swaps++;
+	            i--;
+	        }
+	    }
+	    return swaps;
+	}
+};
